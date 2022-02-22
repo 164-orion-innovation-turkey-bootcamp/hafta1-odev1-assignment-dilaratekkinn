d1685304768c665b12dbafe93559d37f9a8152d1
Dilara Tekin <94602968+dilaratekkinn@users.noreply.github.com>
2022-02-22 18:00:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-const target = 9;
-const nums = [2, 7, 11, 15];
+    target = 9;
+    const nums = [2, 7, 11, 15];
 
-function check_one_number(mIndex) {
+    function check_one_number(mIndex) {
 
-    const mNumber = nums[mIndex];
-    console.log('memory Index:' + mIndex)
-    console.log('Memory Number :' + mNumber);
+        const mNumber = nums[mIndex];
+        console.log('memory Index:' + mIndex)
+        console.log('Memory Number :' + mNumber);
 
-    var output = null;
+        var output = null;
 
-    nums.forEach(function (val, key) {
-            console.log('key of arr:' + key + ' ' + 'value of arr:' + val)
+        nums.forEach(function (val, key) {
+                console.log('key of arr:' + key + ' ' + 'value of arr:' + val)
 
-            if (mNumber == val) {
-                return;
+                if (mNumber == val) {
+                    return;
+                }
+                if (mNumber + val == target) {
+
+                    output = [mNumber, val]
+                    return false;
+
+                }
             }
-            if (mNumber + val == target) {
+        );
 
-                output = [mNumber, val]
-                return false;
-
-            }
+        if (output) {
+            return output;
         }
-    );
+        /**
+         * Check other memory number of arr
+         */
+        if (mIndex <= nums.length - 1) {
+            return check_one_number(mIndex + 1);
 
-    if (output) {
-        return output;
-    }
-    /**
-     * Check other memory number of arr
-     */
-    if (mIndex <= nums.length - 1) {
-        return check_one_number(mIndex + 1);
+        }
 
     }
 
-}
-
-result = check_one_number(0);
-document.write('output:' + result);
+    result = check_one_number(0);
+    console.log(result);
